bacda094ef9c3b9c520fea9c379c4710a0ab76d9
felipesuarezB <felipesuarezbe@hotmail.com>
2025-03-28 22:55:28 -0500

    Login de usuario

--- src/apis/jugador_bp.py
+++ src/apis/jugador_bp.py
@@ -1,12 +1,18 @@
+from datetime import timedelta
 from flask import request, jsonify, make_response
 from werkzeug.security import generate_password_hash
 from marshmallow import ValidationError
 from flask_smorest import Blueprint
 from app import db
-from models.jugador import JugadorSchema
+from models.jugador import JugadorJsonSchema, JugadorSchema
 from services.jugador_service import jugador_service
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 
 jugadores_bp = Blueprint("jugadores", __name__, url_prefix='/api', description="API de jugadores.")
+
+# def generate_new_token(user_id):
+#   token = create_access_token(user_id,expires_delta=timedelta(seconds=3600))
+#   return token
 
 @jugadores_bp.route("/auth/signup", methods=["POST"])
 @jugadores_bp.arguments(JugadorSchema)
@@ -18,3 +24,14 @@ def signup(nuevo_jugador):
 
     return res
     
+@jugadores_bp.route("/auth/login", methods=["POST"])
+@jugadores_bp.arguments(JugadorJsonSchema)
+def login(user_crendentials):
+  result = jugador_service.auth_user(user_crendentials)
+#   token = generate_new_token(result.user_id)
+  res_json = jsonify(result.__dict__)
+
+  res = make_response(res_json, result.code)
+#   res.headers['Authorization'] = f'Bearer {token}'
+
+  return res
