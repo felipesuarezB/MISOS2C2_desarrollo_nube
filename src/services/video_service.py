fc6bbea57a8c1cb038a2a620d14885418bc0bd2d
Santiago Gómez Perdomo <s.gomezp2345@uniandes.edu.co>
2025-03-30 18:27:25 -0500

    total de endpoints finalizados y probados

--- src/services/video_service.py
+++ src/services/video_service.py
@@ -5,7 +5,7 @@ import uuid
 
 from flask import jsonify
 from api_messages.api_errors import InternalServerError
-from api_messages.api_videos import VideoDeleted, VideoFailed, VideoListed, VideoUploaded
+from api_messages.api_videos import VideoDeleted, VideoFailed, VideoListed, VideoUploaded, VideoVoted, VideoRanking
 from database import db
 from models.video import Video, VideoSchema
 
@@ -65,6 +65,18 @@ class VideoService:
 
         return VideoListed(found_videos_json)
     
+    def list_public_videos(self, jwt_payload):
+        
+        found_videos = []
+        try:
+            found_videos = db.session.query(Video).all()
+        except Exception as ex:
+            raise InternalServerError() from ex
+        
+        found_videos_json = [VideoSchema().dump(video) for video in found_videos]
+
+        return VideoListed(found_videos_json)
+
     def get_video(self, id_video):
         id_video_uuid = uuid.UUID(id_video)
         found_video = None
@@ -95,5 +107,33 @@ class VideoService:
         db.session.commit()
 
         return VideoDeleted(id_video)
+    
+    def vote_video(self, id_video):
+        id_video_uuid = uuid.UUID(id_video)
+        found_video = None
+        try:
+            found_video = db.session.query(Video).filter(Video.id == id_video_uuid).first()
+            found_video.vote += 1
+        except Exception as ex:
+            raise InternalServerError() from ex
+
+        if not found_video:
+            return VideoFailed()
+        
+        db.session.commit()
+        return VideoVoted()
+    
+    def list_ranking_videos(self, jwt_payload):
+        found_videos = []
+        try:
+            found_videos = db.session.query(Video).order_by(Video.vote.desc()).all()
+
+        except Exception as ex:
+            raise InternalServerError() from ex
+        
+        found_videos_json = [VideoSchema().dump(video) for video in found_videos]
+
+        return VideoRanking(found_videos_json)
+
     
 video_service = VideoService()
