6c46fdf3cfddf528ee9895bf37543289d5f829ed
felipesuarezB <felipesuarezbe@hotmail.com>
2025-03-29 21:57:27 -0500

    merge from desarrollo

--- src/api_messages/api_jugadores.py
+++ src/api_messages/api_jugadores.py
@@ -1,5 +1,10 @@
 from api_messages.base_api_error import ApiError
 
+class UserAlreadyExists(ApiError):
+  code = 409
+
+  def __init__(self):
+    self.message = "Usuario ya existe."
 
 class JugadorCreado:
   code = 201
@@ -7,4 +12,26 @@ class JugadorCreado:
   def __init__(self, user_id):
     self.message = "Usuario creado exitosamente."
     self.user_id = user_id
+
+class JugadoresList:
+  code = 200
+
+  def __init__(self, found_jugadores):
+    self.jugadores = found_jugadores
     
+class UserAuthFailed(ApiError):
+  code = 401
+
+  def __init__(self):
+    self.message = "Autenticación de usuario no exitosa."
+
+
+class UserAuthSucceed:
+  code = 200
+
+  def __init__(self, user_id, token):
+    self.message = "Autenticación de usuario exitosa."
+    self.user_id = user_id,
+    self.token_type = "Bearer",
+    self.expires_in = 3600,
+    self.token = token
