6cb378bb6857179662fc8c4e0b9d6b04b945161b
Felipe Suárez <f.suarezb@uniandes.edu.co>
2025-04-05 19:43:16 -0500

    Feature/jugador services (#11)
    * ajustes de signin
    
    * Login de usuario
    
    * Creacion de servicios de videos
    
    * merge from desarrollo
    
    * ajustes de subir archivos a S3 Amazon

--- src/services/video_service.py
+++ src/services/video_service.py
@@ -1,6 +1,8 @@
 
 from datetime import datetime
+import boto3
 import os
+from werkzeug.utils import secure_filename
 import uuid
 
 from flask import jsonify
@@ -9,34 +11,37 @@ from api_messages.api_videos import VideoDeleted, VideoFailed, VideoListed, Vide
 from database import db
 from models.video import Video, VideoSchema
 
+# Configuración de S3
+S3_BUCKET = "tu-bucket"
+S3_REGION = "us-east-2"
+S3_ACCESS_KEY = ""
+S3_SECRET_KEY = ""
+
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=S3_ACCESS_KEY,
+    aws_secret_access_key=S3_SECRET_KEY,
+    region_name=S3_REGION
+)
+
 
 class VideoService:
-    UPLOAD_FOLDER = 'D:/OneDrive/Videos/' 
-    ALLOWED_EXTENSIONS = {'mp4'}
-    MAX_FILE_SIZE = 100 * 1024 * 1024  
+    
+    def save_video(selft, uploadVideo):        
+        if not uploadVideo['video_file'] or uploadVideo['video_file'].filename.split('.')[-1].lower() != "mp4":
+            return {"error": "Solo se permiten archivos MP4"}, 400
 
-    def allowed_file(filename):
-        return '.' in filename and filename.rsplit('.', 1)[1].lower() in VideoService.ALLOWED_EXTENSIONS
+        filename = f"{datetime.now().strftime('%Y%m%d%H%M%S')}_{secure_filename(uploadVideo['video_file'].filename)}"
+        s3_client.upload_fileobj(uploadVideo['video_file'], S3_BUCKET, filename, ExtraArgs={"ContentType": "video/mp4"})
 
-    def save_video(selft, uploadVideo):
-        # if file.filename == '' or not VideoService.allowed_file(file.filename):
-        #     raise VideoFailed()
-        
-        # file.seek(0)
-        # if len(file.read()) > VideoService.MAX_FILE_SIZE:
-        #     raise VideoFailed()
-        # file.seek(0)
-        
-        #filename = secure_filename(file.filename)
-        # file_path = os.path.join(VideoService.UPLOAD_FOLDER, filename)
-        # file.save(file_path)
+        video_url = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/{filename}"
         
         video = Video(
             title=uploadVideo['title'], 
             status='subido', 
             uploaded_at=datetime.now(), 
             processed_at=datetime.now(), 
-            processed_url="https://example.com/processed_video.mp4",
+            processed_url=video_url,
             id_jugador=uuid.UUID(uploadVideo["id_jugador"]))
         
         db.session.add(video)
